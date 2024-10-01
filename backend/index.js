c7bd0d37a84c105bbbb4a3630ea9bfa522bd27a6
anfal17 <mohdanfal17@gmail.com>
2024-10-01 16:36:08 +0530

    setup

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+cont 
+
+dotenv.config();
+const port = process.env.PORT;
+const app = express();
+app.use(express.json())
+
+app.use(cors({
+    origin: '*',
+    credentials: true,
+  }));
+
+  //routes
+
+
+  //testing route
+  app.get('/' , (req,res)=>{
+    res.send("Api working");
+  })
+
+  app.listen(port , ()=>{
+    console.log(`server listening on port ${port}`);
+  })
